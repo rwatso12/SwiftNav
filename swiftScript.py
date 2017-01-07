0c13085ed1c97255746750b35c5385408905e001
Ryan Watson <rwatso12@gmail.com>
2017-01-07 04:47:47 -0500

    updates after running pylint ... variable names changed

--- swiftScript.py
+++ swiftScript.py
@@ -1,120 +1,120 @@
 #!/usr/bin/env python
 
 '''
-
 Program to generating the first n Fibonacci numbers F(n), printing ...
 - ... "Buzz" when F(n) is divisible by 3.
 - ... "Fizz" when F(n) is divisible by 5.
 - ... "BuzzFizz" when F(n) is prime.
 - ... the value F(n) otherwise.
 
+
+How to Run ??
+    ./fizzBuzz.py -n 5
 '''
 
 
-__author__   = 'Ryan Watson'
-import sys, os, getopt 
+__author__ = 'Ryan Watson'
+
 from itertools import compress
- 
-#Methods used ::
-
-'''
-Generate Fib sequence using 
-Binet's formual. 
-
-Input :: n --> index to calculate fib. seq. [ int ] 
-Output :: fib number at n [ int ]
-'''
-phi = (1 + 5**0.5) / 2
-def fib(n):
-	return int(round((phi**n - (1-phi)**n) / 5**0.5))
 
 
-'''
-Figure out if number n is divisible by x. 
-Simply done by using mod(n,3) == 0
-
-Input :: n --> number from fib. seq.  [ int ]
-         x --> number to divide n     [ int ]
-'''
-def isDiv(n,x):
-	if( (n % x) == 0 ): 
-		return True 
-	return False
-	
-	
-'''
-Figure out if number is prime. 
-Checks to see is number is only
-divisible by 1 itself.
-
-Input :: number from fib. seq.  [ int ]
-'''
-def isPrime(n):
-	if n < 2:
-		return False 
-	elif n == 2:
-		return True
-	for x in range(2, n):
-		if n % x == 0:
-	    		return False
-	return True
+FIB_CONST = (1 + 5**0.5) / 2
+def fib(num):
+    '''
+    Generate Fib sequence using
+    Binet's formual.
+    Input :: n --> index to calculate fib. seq. [ int ]
+    Output :: fib number at n [ int ]
+    '''
+    return int(round((FIB_CONST**num - (1-FIB_CONST)**num) / 5**0.5))
 
 
-'''
-Calls three functions above to 
-check if current integer is 
-1) Prime
-2) div. by 3
-3) div. by 5
-
-Input :: number from fib. seq.  [ int ]
-Output :: tuple -> { Num [int], Num%3 [bool], Num%5 [bool], isPrime [bool] }
-'''
-def checkNumber(n):
-        numTuple = ( n, isDiv(n,3), isDiv(n,5), isPrime(n) );
-	return numTuple
-
-'''
-Print statement based upon given 
-conditions (ie. Buzz if div. by 3 ...) 
-
-Input :: tuple -> { Num [int], Num%3 [bool], Num%5 [bool], isPrime [bool] }
-'''
-def printFib(numTuple):
-	if (sum(numTuple[1:]) < 1):
-		print numTuple[0]
-		return True
-	printList = ["Buzz","Fizz","BuzzFizz"]	
-	printList = list(compress(printList, numTuple[1:]))
-        print ', '.join(printList)
+def is_div(num, div):
+    '''
+    Figure out if number n is divisible by x.
+    Simply done by using mod(n,3) == 0
+    Input :: n --> number from fib. seq.  [ int ]
+             x --> number to divide n     [ int ]
+    '''
+    if num%div == 0:
         return True
+    return False
 
 
+def is_prime(num):
+    '''
+    Figure out if number is prime.
+    Checks to see is number is only
+    divisible by 1 itself.
+    Input :: number from fib. seq.  [ int ]
+    '''
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    for div in range(2, num):
+        if num % div == 0:
+            return False
+    return True
 
-'''
-   MAIN 
-'''
-def main(argv):
-	from optparse import OptionParser
-	help_text = """
 
+def check_number(num):
+    '''
+    Calls three functions above to
+    check if current integer is
+    1) Prime
+    2) div. by 3
+    3) div. by 5
+    Input :: number from fib. seq.  [ int ]
+    Output :: tuple -> { Num [int], Num%3 [bool], Num%5 [bool], isPrime [bool] }
+    '''
+    num_tuple = (num, is_div(num, 3), is_div(num, 5), is_prime(num))
+    return num_tuple
+
+
+def print_fib(num_tuple):
+    '''
+    Print statement based upon given
+    conditions (ie. Buzz if div. by 3 ...)
+    Input :: tuple -> { Num [int], Num%3 [bool], Num%5 [bool], isPrime [bool] }
+    '''
+    if sum(num_tuple[1:]) < 1:
+        print num_tuple[0]
+        return True
+    print_list = ["Buzz", "Fizz", "BuzzFizz"]
+    print_list = list(compress(print_list, num_tuple[1:]))
+    print ', '.join(print_list)
+    return True
+
+
+def main():
+    '''
+    Program to generating the first n Fibonacci numbers F(n), printing ...
+    - ... "Buzz" when F(n) is divisible by 3.
+    - ... "Fizz" when F(n) is divisible by 5.
+    - ... "BuzzFizz" when F(n) is prime.
+    - ... the value F(n) otherwise.
+    '''
+    from optparse import OptionParser
+    help_text = """
 			Program to generating the first n Fibonacci numbers F(n), printing ...
 			- ... "Buzz" when F(n) is divisible by 3.
 			- ... "Fizz" when F(n) is divisible by 5.
 			- ... "BuzzFizz" when F(n) is prime.
 			- ... the value F(n) otherwise.
-
 		    """
-	parser = OptionParser(usage=help_text)
-	parser.add_option('-n', type="int" ,action='store', dest = "num", help='''Integer used to generate Fib sequence''')
-	(opts, args) = parser.parse_args()
-	for num in range(1,opts.num+1):
-                # generate fib. sequence 
-		fibNum = fib(num)
-		# check number to see if it matches condition
-		numTuple = checkNumber(fibNum)
-		# print statement
-		printFib(numTuple)
+    parser = OptionParser(usage=help_text)
+    parser.add_option('-n', type="int", action='store', dest="num",
+                      help='''Integer used to generate Fib sequence''')
+    opts, _ = parser.parse_args()
+    for num in range(1, opts.num+1):
+        # generate fib. sequence
+        fib_num = fib(num)
+        # check number to see if it matches condition
+        num_tuple = check_number(fib_num)
+        # print statement
+        print_fib(num_tuple)
 
-if __name__=="__main__":
-	main(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
